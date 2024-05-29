126cb507c50cfe137408c249f7b175e45d69b07d
Bartosz <knapikbartek@gmail.com>
2024-05-29 11:23:09 +0200

    Started frontend part, fixed issue with spring and react connection

---
+++ mdb-spring-boot/src/main/java/com/example/mdb_spring_boot/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.mdb_spring_boot.controller;
+
+import com.example.mdb_spring_boot.model.User;
+import com.example.mdb_spring_boot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+}
